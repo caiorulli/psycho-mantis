cd734757cf1d6b5c8a98ea1b75bafaa04bb599d3
Caio Rulli Thomaz <caio@crthomaz.com.br>
2022-03-18 13:25:05 -0300

    initial commit by deps-new

---
+++ test/caiorulli/psycho_mantis_test.clj
@@ -0,0 +1,7 @@
+(ns caiorulli.psycho-mantis-test
+  (:require [clojure.test :refer :all]
+            [caiorulli.psycho-mantis :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
